1b84ad32c3351972d31f8cb7f613f2c5b7ea9423
sx01365860-tech <sx01365860@gmail.com>
2026-08-16 12:42:39 +0000

    Initialize Android native project structure

---
+++ app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.subcreator"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.subcreator"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
